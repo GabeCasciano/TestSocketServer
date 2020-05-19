4a9aca796d2d3f8b3b4b40c9572fdfa791813b05
gabecasciano <gabecasciano@gmail.com>
2020-05-19 16:08:21 -0400

    Mondays Work

---
+++ src/libs/Employee.py
@@ -0,0 +1,29 @@
+import datetime
+
+
+class Employee:
+
+    MASTER_ID = 1
+
+    def __init__(self, Name, Department):
+        self.Name = Name
+        self.ID = Employee.MASTER_ID
+        self.Department = Department
+        self.last_seen = datetime.datetime.now() # last time employee was seen on camera
+        self.present = False # if the employee is currently at work
+
+        print("New Employee: {}, {}, department: {}".format(self.Name, self.ID, self.Department))
+
+        Employee.MASTER_ID += 1
+
+    def check_in(self):
+        self.last_seen = datetime.datetime.now()
+        self.present = True
+
+    def check_out(self):
+        self.last_seen = datetime.datetime.now()
+        self.present = False
+
+    def employee_on_site(self):
+        return (self.last_seen, self.present)
+
