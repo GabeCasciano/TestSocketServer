2c24a839a0d713e3e7b71e413228059d622d1284
unknown <gabecasciano@gmail.com>
2020-05-22 16:45:00 -0700

    server start script

---
+++ run_server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd src/server
+python3 server_runtime.py
--- src/Tests/Test_server_and_classes.py
+++ src/Tests/Test_server_and_classes.py
@@ -3,16 +3,17 @@ from socket import socket
 HOST = 'ec2-3-21-205-199.us-east-2.compute.amazonaws.com'
 PORT = 12345 # 12346 for brandon
 
-socket = socket((HOST, PORT))
+sock = socket()
+sock.connect((HOST, PORT))
 
 # Send the server data packet, to create new employee
 data_packet = str.encode(f"/new_emp,Gabe Casciano,Engineering")
-socket.sendall(data_packet)
-data = socket.recv(1024)
+sock.sendall(data_packet)
+data = sock.recv(1024)
 print(data.decode())
 
 # Send data packet to terminate connection
 data_packet = str.encode(f"/close")
-socket.sendall(data_packet)
-data = socket.recv(1024)
+sock.sendall(data_packet)
+data = sock.recv(1024)
 print(data.decode())
